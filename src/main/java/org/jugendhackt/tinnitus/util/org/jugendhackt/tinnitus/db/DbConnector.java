20e25194cbd67274f97c0f6759e5c873273d39ed
flawnn <baran.peters@lessing-ffm.net>
2018-05-12 09:14:23 +0200

    Added util functions to access Db

--- src/main/java/org/jugendhackt/tinnitus/util/org/jugendhackt/tinnitus/db/DbConnector.java
+++ src/main/java/org/jugendhackt/tinnitus/util/org/jugendhackt/tinnitus/db/DbConnector.java
@@ -1,6 +1,12 @@
 package org.jugendhackt.tinnitus.util.org.jugendhackt.tinnitus.db;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+import org.jugendhackt.tinnitus.util.Cache;
+import org.jugendhackt.tinnitus.util.Tuple;
 
 /**
  * @author Flawn
@@ -8,13 +14,32 @@ import java.util.logging.Logger;
 public class DbConnector {
     private Logger logger = Logger.getLogger(this.getClass().getName());
     private String host;
-    private String port;
+    private String username;
+    private String password;
+    private InfluxDB db;
 
     public DbConnector(String host, String port){
         logger.info("Starting to connect to DB");
     }
 
-    public startConnection(){
+    public void startConnection(){
+        try{
+            db = InfluxDBFactory.connect(host, username, password);
+            db.setDatabase("Tinnitus");
+        } catch(Exception e){
+            logger.info("Connection couldn't be established!");
+            logger.severe(e.getMessage());
+        }
+        logger.info("Connection is established!");
 
     }
+    public void writeToDb(Tuple t){
+        Tuple<String, Integer> tuple = Cache.getInstance().getNextElement();
+        db.write(Point.measurement("noise")
+                .time(Long.valueOf(tuple.getKey()), TimeUnit.MILLISECONDS)
+                .addField("db", tuple.getKey())
+                .tag("geolocation")
+                .build());
+    }
+
 }

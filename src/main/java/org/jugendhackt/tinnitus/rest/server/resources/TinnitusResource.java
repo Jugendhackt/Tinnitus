17da1d605be94c3f8d0ceee3afcb4d5ecf079fc4
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2018-05-12 13:25:37 +0200

    First stable build

--- src/main/java/org/jugendhackt/tinnitus/rest/server/resources/TinnitusResource.java
+++ src/main/java/org/jugendhackt/tinnitus/rest/server/resources/TinnitusResource.java
@@ -1,8 +1,13 @@
 package org.jugendhackt.tinnitus.rest.server.resources;
 
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 
 import org.jugendhackt.tinnitus.util.Cache;
 import org.jugendhackt.tinnitus.util.DataSet;
@@ -11,16 +16,18 @@ import org.jugendhackt.tinnitus.util.Tuple;
 @Path("/tinnitus")
 public class TinnitusResource {
 
+    private Logger log = Logger.getLogger(this.getClass().getName());
+    
     @Path("/recvdata")
     @POST
-    public String recvData(@FormParam("mpid") String mpid,
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void recvData(@FormParam("mpid") String mpid,
             @FormParam("time") String time, @FormParam("data") String data) {
+        log.info("Received: MPID=" + mpid + " TIME=" + time + " DATA=" + data);
         Cache.getInstance()
                 .addElement(new DataSet<String, Integer>(Integer.valueOf(mpid),
                         new Tuple<String, Integer>(time,
                                 Integer.valueOf(data))));
 
-        return time + " " + data;
     }
-
 }

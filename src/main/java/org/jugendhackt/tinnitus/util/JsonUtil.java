72d015a70807303dd81acb04a01c88c2d81d5c1d
Hendrik Steinmetz <hendrik_steinmetz@live.com>
2018-05-13 10:33:23 +0200

    JsonUtil rewrite

--- src/main/java/org/jugendhackt/tinnitus/util/JsonUtil.java
+++ src/main/java/org/jugendhackt/tinnitus/util/JsonUtil.java
@@ -2,74 +2,75 @@ package org.jugendhackt.tinnitus.util;
 
 import static org.jugendhackt.tinnitus.frontend.GeoJsonGenerator.mapper;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.jugendhackt.tinnitus.frontend.models.locations.Locations;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
 /**
  * @author Flawn
  */
 public class JsonUtil {
-    public static void addLocation(String lat, String lng, String id) {
+    private static Locations locs;
+    
+    public static void addLocation(String lat, String lng,
+            String id) {
+        if(locs == null)
+            try {
+                loadJson();
+            }
+            catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        
+        ArrayList<String> insert = new ArrayList<>();
+        insert.add(lat);
+        insert.add(lng);
+        insert.add(id);
+        if (Integer.parseInt(id) < locs.getLocations().size()) {
+            locs.getLocations().set(Integer.parseInt(id), insert);
+        } else {
+            locs.getLocations().add(insert);
+        }
+        
         try {
-            System.out.println("adding");
-            Locations locations;
-
-            if (isFileEmpty("locations.json")) {
-                locations = new Locations();
-            }
-            else {
-                locations = mapper.readValue(Paths.get("locations.json")
-                        .toFile(), Locations.class);
-            }
-
-            ArrayList<ArrayList<String>> ar = locations.getLocations();
-            ArrayList<String> arr = new ArrayList<String>(
-                    Arrays.asList(lat, lng, id));
-
-            if (ar != null) {
-                if (ar.contains(arr)) {
-                    ar.remove(arr);
-                }
-            }
-            else {
-                ar = new ArrayList<ArrayList<String>>();
-            }
-
-            ar.add(arr);
-
-            locations.setLocations(ar);
-
-            mapper.writeValue(
-                    Files.newOutputStream(Paths.get("locations.json")),
-                    locations);
-            System.out.println("finished");
+            saveJson();
         }
         catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-
-    private static boolean isFileEmpty(String path) {
-        try {
-            return new FileInputStream(Paths.get(path)
-                    .toFile()).getChannel()
-                            .size() == 0;
+    
+    private static void loadJson() throws JsonParseException, JsonMappingException, IOException {
+      //LOADING OR CREATING BASED ON THE FILES EXISTANCE
+        if (new File("locations.json").exists()) {
+            locs = mapper.readValue(Paths.get("locations.json")
+                    .toFile(), Locations.class);
         }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
+        else {
+            locs = new Locations();
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return false;
+    }
+    
+    private static void saveJson() throws JsonGenerationException, JsonMappingException, IOException {
+        mapper.writeValue(
+                Files.newOutputStream(Paths.get("locations.json")),
+                locs);
     }
 }
